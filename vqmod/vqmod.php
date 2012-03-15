27fe5de8254bd92ccec97d7bf74fa41181c61082
qphoria@gmail.com <qphoria@gmail.com@f69d930b-bad7-f73c-3b68-bc65bccf09c4>
2012-03-15 18:38:02 +0000

    v2.1.6  - Added "error" attribute to the <file> tag to allow skip, log, or abort of missing file paths.  - Moved main class to top of file to make it easier to find version number.
    git-svn-id: https://vqmod.googlecode.com/svn/trunk@69 f69d930b-bad7-f73c-3b68-bc65bccf09c4

--- vqmod/vqmod.php
+++ vqmod/vqmod.php
@@ -1,4 +1,256 @@
 <?php
+/**
+ * VQMod
+ * @description Main Object used
+ */
+final class VQMod {
+	private $_vqversion = '2.1.6';
+	private $_modFileList = array();
+	private $_mods = array();
+	private $_filesModded = array();
+	private $_cwd = '';
+	private $_doNotMod = array();
+	private $_virtualMode = true;
+
+	public $useCache = false;
+	public $logFilePath = 'vqmod/vqmod.log';
+	public $vqCachePath = 'vqmod/vqcache/';
+	public $protectedFilelist = 'vqmod/vqprotect.txt';
+	public $logging = true;
+	public $log;
+
+	/**
+	 * VQMod::__construct()
+	 *
+	 * @param bool $path File path to use
+	 * @param bool $logging Enable/disabled logging
+	 * @return null
+	 * @description Startup of VQMod
+	 */
+	public function __construct($path = false, $logging = true) {
+		if(!class_exists('DOMDocument')) {
+			die('ERROR - YOU NEED DOMDocument INSTALLED TO USE VQMod');
+		}
+
+		if(!$path){
+			$path = dirname(dirname(__FILE__));
+		}
+		$this->_setCwd($path);
+
+		$this->logging = (bool) $logging;
+		$this->log = new VQModLog($this);
+
+		$this->_getMods();
+		$this->_loadProtected();
+	}
+
+	/**
+	 * VQMod::modCheck()
+	 *
+	 * @param string $sourceFile path for file
+	 * @return string
+	 * @description Checks if a file has modifications and applies them, returning cache files or the file name
+	 */
+	public function modCheck($sourceFile) {
+
+		if(!preg_match('%^([a-z]:)?[\\\\/]%i', $sourceFile)) {
+			$sourcePath = $this->path($sourceFile);
+		} else {
+			$sourcePath = realpath($sourceFile);
+		}
+
+		if(!$sourcePath || is_dir($sourcePath) || in_array($sourcePath, $this->_doNotMod)) {
+			return $sourceFile;
+		}
+
+		$stripped_filename = preg_replace('~^' . preg_quote($this->getCwd(), '~') . '~', '', $sourcePath);
+		$cacheFile = $this->_cacheName($stripped_filename);
+
+		if($this->useCache && file_exists($cacheFile)) {
+			return $cacheFile;
+		}
+
+		if(isset($this->_filesModded[$sourcePath])) {
+			return $this->_filesModded[$sourcePath]['cached'] ? $cacheFile : $sourceFile;
+		}
+
+		$changed = false;
+		$fileHash = sha1_file($sourcePath);
+		$fileData = file_get_contents($sourcePath);
+
+		foreach($this->_mods as $modObject) {
+			foreach($modObject->mods as $path => $mods) {
+				if($this->_checkMatch($path, $sourcePath)) {
+					$modObject->applyMod($mods, $fileData);
+				}
+			}
+		}
+
+		// START QPHORIA CACHELOCK CODE
+		if (sha1($fileData) != $fileHash) {
+			$writePath = $this->_virtualMode ?  $cacheFile : $sourcePath;
+			$cacheLock = false;
+			if ($this->_virtualMode && file_exists($writePath) && ((filemtime($writePath) + 3) >= time())) { $cacheLock = true; $changed = true; }
+			if(!$cacheLock && (!file_exists($writePath) || is_writable($writePath))) {
+				file_put_contents($writePath, $fileData);
+				$changed = true;
+			} else {
+				//file_put_contents('./cachelock.txt', "$writePath \r\n", FILE_APPEND);
+			}
+			//file_put_contents('./cachetotal.txt', "$writePath \r\n", FILE_APPEND);
+		} // END QPHORIA CACHELOCK CODE
+
+		/* Original Code
+		if(sha1($fileData) != $fileHash) {
+			$writePath = $this->_virtualMode ?  $cacheFile : $sourcePath;
+			if(!file_exists($writePath) || is_writable($writePath)) {
+				file_put_contents($writePath, $fileData);
+				$changed = true;
+			}
+		}*/
+
+		$this->_filesModded[$sourcePath] = array('cached' => $changed);
+		return $changed ? $writePath : $sourcePath;
+	}
+
+	/**
+	 * VQMod::path()
+	 *
+	 * @param string $path File path
+	 * @param bool $skip_real If true path is full not relative
+	 * @return bool, string
+	 * @description Returns the full true path of a file if it exists, otherwise false
+	 */
+	public function path($path, $skip_real = false) {
+		$tmp = $this->_cwd . $path;
+		$realpath = $skip_real ? $tmp : realpath($tmp);
+		if(!$realpath) {
+			return false;
+		}
+		if(is_dir($realpath)) {
+			$realpath = rtrim($realpath, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+		}
+		return $realpath;
+	}
+
+	/**
+	 * VQMod::getCwd()
+	 *
+	 * @return string
+	 * @description Returns current working directory
+	 */
+	public function getCwd() {
+		return $this->_cwd;
+	}
+
+	/**
+	 * VQMod::_getMods()
+	 *
+	 * @return null
+	 * @description Gets list of XML files in vqmod xml folder for processing
+	 */
+	private function _getMods() {
+
+		$this->_modFileList = glob($this->path('vqmod/xml/') . '*.xml');
+
+		if($this->_modFileList) {
+			$this->_parseMods();
+		} else {
+			$this->log->write('NO MODS IN USE');
+		}
+	}
+
+	/**
+	 * VQMod::_parseMods()
+	 *
+	 * @return null
+	 * @description Loops through xml files and attempts to load them as VQModObject's
+	 */
+	private function _parseMods() {
+
+		$dom = new DOMDocument('1.0', 'UTF-8');
+		foreach($this->_modFileList as $modFileKey => $modFile) {
+			if(file_exists($modFile)) {
+				if(@$dom->load($modFile)) {
+					$mod = $dom->getElementsByTagName('modification')->item(0);
+					$this->_mods[] = new VQModObject($mod, $modFile, $this);
+				} else {
+					$this->log->write('DOM UNABLE TO LOAD: ' . $modFile);
+				}
+			} else {
+				$this->log->write('FILE NOT FOUND: ' . $modFile);
+			}
+		}
+	}
+
+	/**
+	 * VQMod::_loadProtected()
+	 *
+	 * @return null
+	 * @description Loads protected list and adds them to _doNotMod array
+	 */
+	private function _loadProtected() {
+		$file = $this->path($this->protectedFilelist);
+		if($file && is_file($file)) {
+			$protected = file_get_contents($file);
+			if(!empty($protected)) {
+				$protected = preg_replace('~\r?\n~', "\n", $protected);
+				$paths = explode("\n", $protected);
+				foreach($paths as $path) {
+					$fullPath = $this->path($path);
+					if($fullPath && !in_array($fullPath, $this->_doNotMod)) {
+						$this->_doNotMod[] = $fullPath;
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * VQMod::_cacheName()
+	 *
+	 * @param string $file Filename to be converted to cache filename
+	 * @return string
+	 * @description Returns cache file name for a path
+	 */
+	private function _cacheName($file) {
+		return $this->path($this->vqCachePath) . 'vq2-' . preg_replace('~[/\\\\]+~', '_', $file);
+	}
+
+	/**
+	 * VQMod::_setCwd()
+	 *
+	 * @param string $path Path to be used as current working directory
+	 * @return null
+	 * @description Sets the current working directory variable
+	 */
+	private function _setCwd($path) {
+		$realpath = realpath($path);
+		if(!$realpath) {
+			die('COULDNT RESOLVE CWD REALPATH');
+		}
+		$this->_cwd = rtrim($realpath, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+	}
+
+	/**
+	 * VQMod::_checkMatch()
+	 *
+	 * @param string $modFilePath Modification path from a <file> node
+	 * @param string $checkFilePath File path
+	 * @return bool
+	 * @description Checks a modification path against a file path
+	 */
+	private function _checkMatch($modFilePath, $checkFilePath) {
+		$modFilePath = str_replace('\\', '/', $modFilePath);
+		$checkFilePath = str_replace('\\', '/', $checkFilePath);
+
+		$modFilePath = preg_replace('/([^*]+)/e', 'preg_quote("$1", "~")', $modFilePath);
+		$modFilePath = str_replace('*', '[^/]*', $modFilePath);
+		$return = (bool) preg_match('~^' . $modFilePath . '$~', $checkFilePath);
+		return $return;
+
+	}
+}
 
 /**
  * VQModLog
@@ -274,14 +526,23 @@ class VQModObject {
 
 		foreach($files as $file) {
 			$fileToMod = $file->getAttribute('name');
+			$error = ($file->hasAttribute('error')) ? $file->getAttribute('error') : 'log';
 			$fullPath = $this->_vqmod->path($fileToMod);
 
 			if(!$fullPath){
 				if(strpos($fileToMod, '*') !== false) {
 					$fullPath = $this->_vqmod->getCwd() . $fileToMod;
 				} else {
-					$this->_vqmod->log->write('Could not resolve path for [' . $fileToMod . ']', $this);
-					continue;
+					if ($error == 'log' || $error == 'abort') {
+						$skip = ($error == 'log') ? ' (SKIPPED)' : ' (ABORTING MOD)';
+						$this->_vqmod->log->write('Could not resolve path for [' . $fileToMod . ']' . $skip, $this);
+					}
+
+					if ($error == 'log' || $error == 'skip') {
+						continue;
+					} elseif ($error == 'abort') {
+						return false;
+					}
 				}
 			}
 
@@ -401,257 +662,4 @@ class VQSearchNode extends VQNode {
  * @description Object for the <add> xml tags
  */
 class VQAddNode extends VQNode {
-}
-
-/**
- * VQMod
- * @description Main Object used
- */
-final class VQMod {
-	public $useCache = false;
-	public $logFilePath = 'vqmod/vqmod.log';
-	public $vqCachePath = 'vqmod/vqcache/';
-	public $protectedFilelist = 'vqmod/vqprotect.txt';
-	public $logging = true;
-	public $log;
-
-	private $_vqversion = '2.1.5';
-	private $_modFileList = array();
-	private $_mods = array();
-	private $_filesModded = array();
-	private $_cwd = '';
-	private $_doNotMod = array();
-	private $_virtualMode = true;
-
-	/**
-	 * VQMod::__construct()
-	 *
-	 * @param bool $path File path to use
-	 * @param bool $logging Enable/disabled logging
-	 * @return null
-	 * @description Startup of VQMod
-	 */
-	public function __construct($path = false, $logging = true) {
-		if(!class_exists('DOMDocument')) {
-			die('ERROR - YOU NEED DOMDocument INSTALLED TO USE VQMod');
-		}
-
-		if(!$path){
-			$path = dirname(dirname(__FILE__));
-		}
-		$this->_setCwd($path);
-
-		$this->logging = (bool) $logging;
-		$this->log = new VQModLog($this);
-
-		$this->_getMods();
-		$this->_loadProtected();
-	}
-
-	/**
-	 * VQMod::modCheck()
-	 *
-	 * @param string $sourceFile path for file
-	 * @return string
-	 * @description Checks if a file has modifications and applies them, returning cache files or the file name
-	 */
-	public function modCheck($sourceFile) {
-
-		if(!preg_match('%^([a-z]:)?[\\\\/]%i', $sourceFile)) {
-			$sourcePath = $this->path($sourceFile);
-		} else {
-			$sourcePath = realpath($sourceFile);
-		}
-
-		if(!$sourcePath || is_dir($sourcePath) || in_array($sourcePath, $this->_doNotMod)) {
-			return $sourceFile;
-		}
-
-		$stripped_filename = preg_replace('~^' . preg_quote($this->getCwd(), '~') . '~', '', $sourcePath);
-		$cacheFile = $this->_cacheName($stripped_filename);
-
-		if($this->useCache && file_exists($cacheFile)) {
-			return $cacheFile;
-		}
-
-		if(isset($this->_filesModded[$sourcePath])) {
-			return $this->_filesModded[$sourcePath]['cached'] ? $cacheFile : $sourceFile;
-		}
-
-		$changed = false;
-		$fileHash = sha1_file($sourcePath);
-		$fileData = file_get_contents($sourcePath);
-
-		foreach($this->_mods as $modObject) {
-			foreach($modObject->mods as $path => $mods) {
-				if($this->_checkMatch($path, $sourcePath)) {
-					$modObject->applyMod($mods, $fileData);
-				}
-			}
-		}
-
-		// START QPHORIA CACHELOCK CODE
-		if (sha1($fileData) != $fileHash) {
-			$writePath = $this->_virtualMode ?  $cacheFile : $sourcePath;
-			$cacheLock = false;
-			if ($this->_virtualMode && file_exists($writePath) && ((filemtime($writePath) + 3) >= time())) { $cacheLock = true; $changed = true; }
-			if(!$cacheLock && (!file_exists($writePath) || is_writable($writePath))) {
-				file_put_contents($writePath, $fileData);
-				$changed = true;
-			} else {
-				//file_put_contents('./cachelock.txt', "$writePath \r\n", FILE_APPEND);
-			}
-			//file_put_contents('./cachetotal.txt', "$writePath \r\n", FILE_APPEND);
-		} // END QPHORIA CACHELOCK CODE
-
-		/* Original Code
-		if(sha1($fileData) != $fileHash) {
-			$writePath = $this->_virtualMode ?  $cacheFile : $sourcePath;
-			if(!file_exists($writePath) || is_writable($writePath)) {
-				file_put_contents($writePath, $fileData);
-				$changed = true;
-			}
-		}*/
-
-		$this->_filesModded[$sourcePath] = array('cached' => $changed);
-		return $changed ? $writePath : $sourcePath;
-	}
-
-	/**
-	 * VQMod::path()
-	 *
-	 * @param string $path File path
-	 * @param bool $skip_real If true path is full not relative
-	 * @return bool, string
-	 * @description Returns the full true path of a file if it exists, otherwise false
-	 */
-	public function path($path, $skip_real = false) {
-		$tmp = $this->_cwd . $path;
-		$realpath = $skip_real ? $tmp : realpath($tmp);
-		if(!$realpath) {
-			return false;
-		}
-		if(is_dir($realpath)) {
-			$realpath = rtrim($realpath, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
-		}
-		return $realpath;
-	}
-
-	/**
-	 * VQMod::getCwd()
-	 *
-	 * @return string
-	 * @description Returns current working directory
-	 */
-	public function getCwd() {
-		return $this->_cwd;
-	}
-
-	/**
-	 * VQMod::_getMods()
-	 *
-	 * @return null
-	 * @description Gets list of XML files in vqmod xml folder for processing
-	 */
-	private function _getMods() {
-
-		$this->_modFileList = glob($this->path('vqmod/xml/') . '*.xml');
-
-		if($this->_modFileList) {
-			$this->_parseMods();
-		} else {
-			$this->log->write('NO MODS IN USE');
-		}
-	}
-
-	/**
-	 * VQMod::_parseMods()
-	 *
-	 * @return null
-	 * @description Loops through xml files and attempts to load them as VQModObject's
-	 */
-	private function _parseMods() {
-
-		$dom = new DOMDocument('1.0', 'UTF-8');
-		foreach($this->_modFileList as $modFileKey => $modFile) {
-			if(file_exists($modFile)) {
-				if(@$dom->load($modFile)) {
-					$mod = $dom->getElementsByTagName('modification')->item(0);
-					$this->_mods[] = new VQModObject($mod, $modFile, $this);
-				} else {
-					$this->log->write('DOM UNABLE TO LOAD: ' . $modFile);
-				}
-			} else {
-				$this->log->write('FILE NOT FOUND: ' . $modFile);
-			}
-		}
-	}
-
-	/**
-	 * VQMod::_loadProtected()
-	 *
-	 * @return null
-	 * @description Loads protected list and adds them to _doNotMod array
-	 */
-	private function _loadProtected() {
-		$file = $this->path($this->protectedFilelist);
-		if($file && is_file($file)) {
-			$protected = file_get_contents($file);
-			if(!empty($protected)) {
-				$protected = preg_replace('~\r?\n~', "\n", $protected);
-				$paths = explode("\n", $protected);
-				foreach($paths as $path) {
-					$fullPath = $this->path($path);
-					if($fullPath && !in_array($fullPath, $this->_doNotMod)) {
-						$this->_doNotMod[] = $fullPath;
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * VQMod::_cacheName()
-	 *
-	 * @param string $file Filename to be converted to cache filename
-	 * @return string
-	 * @description Returns cache file name for a path
-	 */
-	private function _cacheName($file) {
-		return $this->path($this->vqCachePath) . 'vq2-' . preg_replace('~[/\\\\]+~', '_', $file);
-	}
-
-	/**
-	 * VQMod::_setCwd()
-	 *
-	 * @param string $path Path to be used as current working directory
-	 * @return null
-	 * @description Sets the current working directory variable
-	 */
-	private function _setCwd($path) {
-		$realpath = realpath($path);
-		if(!$realpath) {
-			die('COULDNT RESOLVE CWD REALPATH');
-		}
-		$this->_cwd = rtrim($realpath, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
-	}
-
-	/**
-	 * VQMod::_checkMatch()
-	 *
-	 * @param string $modFilePath Modification path from a <file> node
-	 * @param string $checkFilePath File path
-	 * @return bool
-	 * @description Checks a modification path against a file path
-	 */
-	private function _checkMatch($modFilePath, $checkFilePath) {
-		$modFilePath = str_replace('\\', '/', $modFilePath);
-		$checkFilePath = str_replace('\\', '/', $checkFilePath);
-
-		$modFilePath = preg_replace('/([^*]+)/e', 'preg_quote("$1", "~")', $modFilePath);
-		$modFilePath = str_replace('*', '[^/]*', $modFilePath);
-		$return = (bool) preg_match('~^' . $modFilePath . '$~', $checkFilePath);
-		return $return;
-
-	}
 }
